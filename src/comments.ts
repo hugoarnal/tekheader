d18d9e507ccf179356c7760a3424e0197f482db0
Hugo ARNAL <hugo@hugoarnal.com>
2026-01-17 01:35:20 +0100

    ref: clean the entire comments.ts file

--- src/comments.ts
+++ src/comments.ts
@@ -1,63 +1,71 @@
 import * as vscode from "vscode";
 
-const ct = {
-    slash: {
-        top: "/*",
-        middle: "**",
-        bottom: "*/",
-    },
-    hash: {
-        top: "##",
-        middle: "##",
-        bottom: "##",
-    },
-    dash: {
-        top: "{-",
-        middle: "--",
-        bottom: "-}",
-    },
-    semicolon: {
-        top: ";;",
-        middle: ";;",
-        bottom: ";;",
-    },
-};
+class DefaultComment {
+    top: string;
+    middle: string;
+    bottom: string;
+    languages: ReadonlyArray<string>;
+
+    constructor(
+        top: string,
+        middle: string,
+        bottom: string,
+        languages: ReadonlyArray<string> = [],
+    ) {
+        this.top = top;
+        this.middle = middle;
+        this.bottom = bottom;
+        this.languages = languages;
+    }
+
+    get dict() {
+        return { top: this.top, middle: this.middle, bottom: this.bottom };
+    }
+
+    toString() {
+        return `${this.top}, ${this.middle}, ${this.bottom}`;
+    }
+}
 
 export async function getCommentSymbols(language: string) {
-    const slashLang = [
-        "c",
-        "cpp",
-        "css",
-        "go",
-        "java",
-        "javascript",
-        "typescript",
-        "rust",
-    ];
-    const hashLang = ["dockerfile", "makefile", "python", "shellscript"];
-    const dashLang = ["haskell"];
-    const semicolonLang = ["ini", "lua"];
+    const symbols = {
+        slash: new DefaultComment("/*", "**", "*/", [
+            "c",
+            "cpp",
+            "css",
+            "go",
+            "java",
+            "javascript",
+            "typescript",
+            "rust",
+        ]),
+        hash: new DefaultComment("##", "##", "##", [
+            "dockerfile",
+            "makefile",
+            "python",
+            "shellscript",
+        ]),
+        dash: new DefaultComment("{-", "--", "-}", ["haskell"]),
+        semicolon: new DefaultComment(";;", ";;", ";;", ["ini", "lua"]),
+    };
 
-    if (slashLang.includes(language)) {
-        return ct.slash;
-    }
-    if (hashLang.includes(language)) {
-        return ct.hash;
-    }
-    if (dashLang.includes(language)) {
-        return ct.dash;
-    }
-    if (semicolonLang.includes(language)) {
-        return ct.semicolon;
+    for (const symbolType of Object.keys(symbols)) {
+        const symbol = symbols[
+            symbolType as keyof Object
+        ] as unknown as DefaultComment;
+
+        if (symbol.languages.includes(language)) {
+            return symbol.dict;
+        }
     }
 
     // If language isn't in defined language list
     let picker = await vscode.window.showQuickPick(
         [
-            `Slash (C, JS, TS...) (${ct.slash.top}, ${ct.slash.middle}, ${ct.slash.bottom})`,
-            `Hash (Dockerfile, Makefile, Python...) (${ct.hash.top}, ${ct.hash.middle}, ${ct.hash.bottom})`,
-            `Dash (Haskell) (${ct.dash.top}, ${ct.dash.middle}, ${ct.dash.bottom})`,
-            `Semicolon (ini, lua) (${ct.semicolon.top}, ${ct.semicolon.middle}, ${ct.semicolon.bottom})`,
+            `Slash (C, JS, TS...) (${symbols.slash.toString()})`,
+            `Hash (Dockerfile, Makefile, Python...) (${symbols.hash.toString()})`,
+            `Dash (Haskell) (${symbols.dash.toString()})`,
+            `Semicolon (ini, lua) (${symbols.semicolon.toString()})`,
         ],
         {
             title: "Couldn't find the language in the defined languages list. Please select your comment type below",
@@ -66,18 +74,17 @@ export async function getCommentSymbols(language: string) {
     if (!picker) {
         return undefined;
     }
+
     picker = picker.toLowerCase();
-    if (picker.includes("slash")) {
-        return ct.slash;
-    }
-    if (picker.includes("hash")) {
-        return ct.hash;
-    }
-    if (picker.includes("dash")) {
-        return ct.dash;
-    }
-    if (picker.includes("semicolon")) {
-        return ct.semicolon;
+
+    for (const symbolType of Object.keys(symbols)) {
+        const symbol = symbols[
+            symbolType as keyof Object
+        ] as unknown as DefaultComment;
+
+        if (picker.includes(symbolType.toLowerCase())) {
+            return symbol.dict;
+        }
     }
     return undefined;
 }
